6d1df3bd68e68d2323785cd0f5cb78961efe8254
Jennifer Asimit <ja628@cam.ac.uk>
2018-02-06 00:39:10 +0000

    instructions and main functions

---
+++ R/ARIELqcc.R
@@ -0,0 +1,236 @@
+
+ariel.data.fn <- function(pheno,allgenodata,chr,start,end,nc=1) {
+# outputs the data required for runnning amelia function for a given region (start,end)
+# chr is the chromosome number
+# nc=1 for non-consensus qs analysis, othrwise consensus qs analysis
+
+# input: 
+# pheno: N-vector of case-control status (1=case, 0=controls)
+# genodata: columns are chr, position,reference allele, alternative allele, SNPquality,
+#          set of 3 columns for indiv i (allele 1, allele 2, Gquality)
+
+# G is K by N matrix of genotype scores
+# genotype scores: 0,1,2 for number of minor alleles
+# Q[k] SNP quality score at SNP k (based on reads from all indivs)
+
+
+pheno <- unlist(pheno)
+#Nold <- length(pheno)
+
+#indrm <- which(pheno<0)
+#if(length(indrm)>0)     {
+# pheno <- pheno[-indrm]
+#}
+
+N <- length(pheno)
+#print(indrm)
+n1 <- sum(pheno==1)
+n2 <- N-n1
+
+print(c("N",N))
+
+names(allgenodata)[1:2] <- c("chr","pos")
+chrgeno <- allgenodata[allgenodata$chr==chr,]
+
+genodata <- chrgeno[chrgeno$pos >= start & chrgeno$pos <= end,]
+
+print(dim(genodata))
+
+if (dim(genodata)[1]==0){
+#	return(list(pheno=pheno,geno=genodata,qSNP=0,qgeno=0))
+out <- list(pheno=pheno,geno=genodata,qgeno=0)
+}
+
+if (dim(genodata)[1]>0) {
+names(genodata)[1:4] <- c("chr","pos","ref","alt")
+
+if(nc==0) {
+# SNP quality scores
+Q <- genodata[,5] # SNP scores: 1 for each variant
+Q <- 1-10^(-Q/10) # transform phred score to prob of correct
+
+Qrm <- which(Q==0) # remove data with SNP quality = 0
+if(length(Qrm)>0) {
+Q <- Q[-Qrm]
+genodata <- genodata[-Qrm,]
+}
+q <- Q
+}
+#####
+
+K <- dim(genodata)[1]
+ref <- genodata$ref
+print(c("K",K))
+
+if(nc==1) {
+# genotype quality scores 
+q <- genodata[,(8+3*(0:(N-1)))] # K by N matrix
+q <- matrix(as.numeric(unlist(q)),nrow=K,ncol=N)
+q <- 1-10^(-q/10) # transform phred scores to probs
+print(c("q",dim(q)))
+}
+#q <- q[,-indrm]
+
+# haplotypes
+g1 <- genodata[,(6+3*(0:(N-1)))] # K by N,  1=alt allele, 0=ref allele
+g2 <- genodata[,(7+3*(0:(N-1)))]
+#print(dim(g1))
+#g1 <- g1[,-indrm]
+#g2 <- g2[,-indrm]
+
+
+
+MAF.fn <- function(g1,g2,j)	{
+# g1j is vector of 1{1st allele = alt allele} for variant j
+ g1j <- g1[j,]
+ g2j <- g2[j,]
+ mj <- sum(g1j!="NA",na.rm=TRUE) # no. of typed indivs at variant j
+ alt.count <- sum(g1j,na.rm=TRUE) + sum(g2j,na.rm=TRUE)
+ ref.count <- mj*2 - alt.count
+ if(ref.count <= alt.count) {
+  maf <- ref.count/(2*mj) # each indiv has 2 alleles at a variant
+  a <- 1 # indicator that minor allele = ref allele
+			   }					
+if(ref.count > alt.count) {
+  maf <- alt.count/(2*mj)
+  a <- 0 # indicator that minor allele = ref allele
+			   }
+return(c(maf,a))					
+				}
+
+j.mat <- matrix(1:K,nrow=K)
+
+MAF.out <-apply(j.mat,1,MAF.fn,g1=g1,g2=g2)
+
+maf <- MAF.out[1,]
+minor.ref <- MAF.out[2,]
+# where minor.ref=1 have 1{minor al = ref al} so change g1,g2 coding
+indm <- which(minor.ref==1) 
+# where minor.ref=0 have minor allele = ref allele so no change to g1,g2 coding
+
+print(summary(maf))
+
+if(length(indm)>0){
+
+g1new <- g1
+g1new[indm,] <- 1-g1[indm,]
+g1 <- g1new; rm(g1new)  # 1=minor allele
+
+g2new <- g2
+g2new[indm,] <- 1-g2[indm,]
+g2 <- g2new; rm(g2new) # 1=minor allele
+}
+
+ind0 <- which(maf==0) # remove non-polymorphic SNPs
+if(length(ind0)>0) 	{
+ maf <- maf[-ind0]
+ g1 <- g1[-ind0,]
+ g2 <- g2[-ind0,]
+ print(dim(q))
+ if(nc==1) q <- q[-ind0,]
+ if(nc==0) {q <- q[-ind0]; q<- matrix(rep(q,N),nrow=length(q),ncol=N) }
+					}
+G <- g1+g2
+
+print(dim(q))
+out <- list(pheno=pheno,geno=G,qgeno=q)
+}
+
+
+#return(list(pheno=pheno,geno=G,qSNP=Q))
+return(out)
+}
+
+
+
+ARIELq.fn <- function(y,g,qgeno,mafrv) {
+# n indivs, m variants
+# y= case(1)-control(0) status
+# assume g (genotype scores) has m rows and n columns
+# assume high quality score is high quality
+# mafrv is cut-off for variants included in collapsing method
+
+if(max(y)==2) { # cases=1,controls=2 instead of 1,0
+y[y==2] <- 0
+}
+
+m <- dim(g)[1]
+n <- dim(g)[2]
+
+MAF.fn <- function(g,j)	{
+# g is m by n matrix of genotypes=minor allele counts {0,1,2}
+ gj <- g[j,]
+ nj <- sum(gj!="NA",na.rm=TRUE) # no. of typed indivs at variant j
+ maf <- sum(gj,na.rm=TRUE)/(2*nj)
+return(maf)					
+			}
+
+cases <- which(y==1)
+controls <- (1:n)[-cases]
+j.mat <-matrix(1:m,nrow=m)
+MAFcases <-apply(j.mat,1,MAF.fn,g=g[,cases]) 
+
+ind.rvs <- which(MAFcases < mafrv)
+
+if(length(ind.rvs)<2)	{
+out <- rep("NA",8)
+						}
+
+if(length(ind.rvs)>1)	{
+print("in loop")
+print(dim(g)) 
+print(dim(qgeno))
+grv <- g[ind.rvs,] # genotypes of rare variants in cases
+qrv <- qgeno[ind.rvs,]
+
+mrv <- dim(grv)[1]
+print(c("mrv=",mrv))
+
+# original collapsing method
+r.fn <- function(g) return(sum(g>0,na.rm=TRUE)) # for indiv i, count of number of variants with at least 1 minor allele
+len.fn <- function(g) return(sum(g!="NA",na.rm=TRUE)) # number of variants successfully typed (no filtering on SNP quality)
+
+r <- apply(grv,2,r.fn) # n vector
+#z <- 1*(r>0)
+ni <- apply(grv,2,len.fn) 
+prop <- r/ni
+
+print("reg")	
+oreg <- glm(y~prop,family=binomial)
+o2pval <- summary(oreg)$coefficients[2,4]
+o2beta <- summary(oreg)$coefficients[2,1]
+se <- summary(oreg)$coefficients[2,2]
+o2OR <- exp(o2beta)
+o2ORadj <- exp(o2beta/mrv)
+Lo2adj <- exp(o2beta/mrv-1.96*se/mrv)	
+Uo2adj <- exp(o2beta/mrv+1.96*se/mrv)
+								
+# quality score weighted proportion of number of variants with at least 1 minor allele, for indiv i
+wp.fn <- function(g,qgeno,i) return(sum((g[,i]>0)*qgeno[,i],na.rm=TRUE)/sum(qgeno[,i],na.rm=TRUE)) 
+i.mat <- matrix(1:n,nrow=n)
+wp <- apply(i.mat,1,wp.fn,g=grv,qgeno=qrv) # n vector
+							
+								
+wreg <- glm(y~wp,family=binomial)
+w2pval <- summary(wreg)$coefficients[2,4]
+w2beta <- summary(wreg)$coefficients[2,1]
+se <- summary(wreg)$coefficients[2,2]
+w2OR <- exp(w2beta)
+w2ORadj <- exp(w2beta/mrv)
+Lw2adj <- exp(w2beta/mrv-1.96*se/mrv)	
+Uw2adj <- exp(w2beta/mrv+1.96*se/mrv)
+
+# output = number of rare snps,original and weighted pvalues, ORs, lower 95% confidence limits, upper 95% confidence limits
+
+#out <- c(o1pval,o2pval,w1pval,w2pval,o1OR,o2OR,w1OR,w2OR,Lo1,Lo2,Lw1,Lw2,Uo1,Uo2,Uw1,Uw2)
+out <- c(mrv,o2pval,w2pval,o2ORadj,w2ORadj,Lo2adj,Lw2adj,Uo2adj,Uw2adj,o2OR,w2OR)
+
+#write.table(matrix(c(o1pval,o2pval,w1pval,w2pval,o1OR,o2OR,w1OR,w2OR),nrow=1),sep="\t",row.names=FALSE,append=FALSE,file=pfile,col.names=FALSE)
+
+	}
+return(out)
+}
+
+######
+
+
---
+++ R/ARIELqcc_script_general.R
@@ -0,0 +1,64 @@
+args           = (commandArgs(TRUE));
+chr            = as.numeric(args[1]);
+mafrv          = as.numeric(args[2]);
+genotype.file  = args[3];# converted vcf file; output from vcf_format_change_v2.pl
+phenotype.file = args[4];
+gene.file      = args[5];
+output.file    = args[6];
+nc				= args[7];
+
+cat("Input parameters:\n","chr              = ",chr,"\n","mafrv            = ",mafrv,"\n","genotype file    = ",genotype.file,"\n","phenotype file   = ",phenotype.file,"\n","gene file        = ",gene.file,"\n","output file name = ",output.file,"\n",sep="");
+
+source("ARIELqcc.R") # ARIEL for SNP quality incorporation, case-control data
+
+#fregions <- "gene_info" # 4 columns: gene.name, chr, start, end
+regions     <- read.table(gene.file);#,quote="",header=FALSE,sep="\t");
+chr.regions <- regions[regions[,2]==chr,];
+starts      <- chr.regions[,3];
+ends        <- chr.regions[,4];
+nregions    <- length(ends);
+rm(regions); gc();
+
+#fpheno <- paste("pheno.txt",sep="");
+
+pheno <- read.table(phenotype.file);#,quote="",header=FALSE,sep="\t");
+pheno <- unlist(pheno[,6])  # column 6 of PED file is phenotype
+# cases=1,controls=0
+if(max(pheno==2)) { # cases=2, controls=1
+pheno <- pheno-1;
+}
+
+idlength <- length(pheno)*3;
+
+#genofile <- paste("vcf_converted.txt",sep="");
+genodata <- read.table(genotype.file,na.strings=".",colClasses=c('character','numeric',rep('character',3),rep('numeric',idlength)));#,header=FALSE,quote="",sep="\t"); 
+genodata <- genodata[,-3]; # rm rsIDs column b/c not needed for analysis
+chrgenodata <- genodata[genodata[,1]==chr,];
+rm(genodata);
+gc();
+
+
+output <-file(output.file,"w"); 
+
+for (i in 1:nregions)
+  {
+    data <- ariel.data.fn(pheno,chrgenodata,chr=chr,start=starts[i],end=ends[i],nc=nc);
+    nsnps <- length(data$geno[,1]);
+    gene <- c(as.character(chr.regions[i,1]),nsnps,chr,starts[i],ends[i]);
+    print(c(i,gene,dim(data$geno)[1]));
+    if (dim(data$geno)[1]<=1)
+      {
+        print("Geno is null!");
+      }
+    else
+      {
+        probs <- ARIELq.fn(y=data$pheno,g=data$geno,qgeno=data$qgeno,mafrv=mafrv);
+        print(probs);
+        cat(gene,probs,sep="\t",file=output);
+        cat("\n",file=output);
+      }
+  }
+close(output);				
+
+q(save="no");
+
